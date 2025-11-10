4b85b9ae5d2555062d0e198aec17c136203bda43
ElenaTu <elena3203@gmail.com>
2025-11-11 02:06:51 +0800

    style: 溢出視窗調整

--- src/App.vue
+++ src/App.vue
@@ -30,21 +30,19 @@ body {
   margin: 0;
   padding: 0;
   width: 100%;
-  height: 100%;
-  background-color: #000;
-  overscroll-behavior: none;
-  touch-action: manipulation;
-  overflow: auto; /* 若要固定畫面改成 hidden */
+  min-height: 100vh;
+  overflow-y: auto;    /* 允許垂直捲軸 */
+  overflow-x: auto;    /* 允許水平捲軸 */
+  background: #000;
 }
 
 /* 主容器固定/最小尺寸 */
-  .site {
-  min-width: 900px;     /* 限定最小寬度 */
-  min-height: 900px;     /* 限定最小高度 */
-  margin: 0 auto;        /* 視窗大時置中 */
-  box-sizing: border-box;
+.site {
+  min-width: 1200px;
+  min-height: 900px;   /* 強制最小高度 */
   background: #f7f7f7;
-  }
+  box-sizing: border-box;
+}
 
 * {
   font-family: 'Swei B2 Sugar CJK TC', Avenir, Helvetica, Arial, sans-serif;
@@ -59,7 +57,7 @@ body {
   justify-content: center;
   align-items: flex-start;
   width: 100%;
-  height: 100%;
+  min-height: 100vh;
   background-color: #000;
 }
 </style>
